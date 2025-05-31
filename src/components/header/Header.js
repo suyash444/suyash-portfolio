b01a87cbfef4b51e82458dffc20422a127d0c276
Suyash Singh <singhsuyash444@gmail.com>
2025-05-31 17:58:04 +0200

    Update header

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,5 +1,4 @@
 import React from "react";
-import Headroom from "react-headroom";
 import "./Header.scss";
 import {
     greeting,
@@ -14,55 +13,59 @@ function Header() {
     const viewResume = resumeSection.display;
 
     return (
-        <Headroom>
-            <header className="header" role="navigation" aria-label="Main Navigation">
-                <a href="/" className="logo" aria-label="Homepage">
-                    <span className="grey-color">&lt;</span>
-                    <span className="logo-name">{greeting.username}</span>
-                    <span className="grey-color">/&gt;</span>
-                </a>
+        <header className="header" role="navigation" aria-label="Main Navigation">
+            <a href="/" className="logo" aria-label="Homepage">
+                <span className="grey-color">&lt;</span>
+                <span className="logo-name">{greeting.username}</span>
+                <span className="grey-color">/&gt;</span>
+            </a>
 
-                {/* Hamburger Menu Toggle */}
-                <input
-                    className="menu-btn"
-                    type="checkbox"
-                    id="menu-btn"
-                    aria-label="Toggle navigation menu"
-                />
-                <label className="menu-icon" htmlFor="menu-btn">
-                    <span className="navicon" />
-                </label>
+            {/* Hamburger Menu Toggle */}
+            <input
+                type="checkbox"
+                className="menu-btn"
+                id="menu-btn"
+                aria-label="Toggle navigation menu"
+            />
+            <label className="menu-icon" htmlFor="menu-btn">
+                <span className="navicon"></span>
+            </label>
 
-                {/* Navigation Menu */}
-                <ul className="menu" role="menubar">
-                    {viewSkills && (
-                        <li role="none">
-                            <a href="#skills" role="menuitem">Skills</a>
-                        </li>
-                    )}
-                    {viewExperience && (
-                        <li role="none">
-                            <a href="#experience" role="menuitem">Work Experiences</a>
-                        </li>
-                    )}
-                    {viewResume && (
-                        <li role="none">
-                            <a
-                                href={resumeSection.resumeLink}
-                                target="_blank"
-                                rel="noopener noreferrer"
-                                role="menuitem"
-                            >
-                                Resume
-                            </a>
-                        </li>
-                    )}
+            {/* Navigation Menu */}
+            <ul className="menu" role="menubar">
+                {viewSkills && (
                     <li role="none">
-                        <a href="#contact" role="menuitem">Contact Me</a>
+                        <a href="#skills" role="menuitem">
+                            Skills
+                        </a>
                     </li>
-                </ul>
-            </header>
-        </Headroom>
+                )}
+                {viewExperience && (
+                    <li role="none">
+                        <a href="#experience" role="menuitem">
+                            Work Experiences
+                        </a>
+                    </li>
+                )}
+                {viewResume && (
+                    <li role="none">
+                        <a
+                            href={resumeSection.resumeLink}
+                            target="_blank"
+                            rel="noopener noreferrer"
+                            role="menuitem"
+                        >
+                            Resume
+                        </a>
+                    </li>
+                )}
+                <li role="none">
+                    <a href="#contact" role="menuitem">
+                        Contact Me
+                    </a>
+                </li>
+            </ul>
+        </header>
     );
 }
 
